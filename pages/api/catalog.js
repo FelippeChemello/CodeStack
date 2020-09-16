4612660d1c707fcd18f52fedb231909112b04606
Felippe <felippe.chemello@kinghost.com.br>
2020-09-15 21:57:18 -0300

    ORDER BY created_at DESC (Mais recentes primeiro)

--- pages/api/catalog.js
+++ pages/api/catalog.js
@@ -1,7 +1,7 @@
 const db = require('../../config/database')
 
 export default async (request, response) => {
-    const res = await db.query("SELECT * FROM portfolio")
+    const res = await db.query("SELECT * FROM portfolio ORDER BY created_at DESC")
 
     response.send(res.rows)
 }
