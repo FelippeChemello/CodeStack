fc83e5f448bf7fa7ef5365fcbceddb123127d262
Felippe <felippe.chemello@kinghost.com.br>
2020-10-20 11:04:34 -0300

    Add propelleradas

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-            <script data-ad-client="ca-pub-6917058823962730" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script
+                        data-ad-client='ca-pub-6917058823962730'
+                        async
+                        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+                    ></script>
+                    <meta name='propeller' content='b2b4f8e0765fe19986f63477fd4c9e04'></meta>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
